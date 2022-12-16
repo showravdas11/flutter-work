a226855d20ff03bfa72369f68ea144a2e3f0cfac
showravdas11 <showravdas11@gmail.com>
2022-12-16 23:58:50 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/wrong/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wrong
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/sh.dart
@@ -0,0 +1,30 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: Scaffold(
+//           backgroundColor: Colors.teal,
+//           body: ListView(
+//             children: <Widget>[
+//               Card(
+//                 child: ListTile(
+//                   title: Text('One-line with trailing widget'),
+//                   trailing: Icon(Icons.more_vert),
+//                   leading: CircleAvatar(
+//                     child: Icon(Icons.message),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ));
+//   }
+// }
